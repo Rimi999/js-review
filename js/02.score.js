a35942a0da4d589b89e59a5209d33a6aa0f0cd99
Rimi999 <all_wom@naver.com>
2021-06-02 16:39:06 +0900

    trim

--- js/02.score.js
+++ js/02.score.js
@@ -11,20 +11,23 @@ var scores = []
 /***************** 이벤트 콜백 *******************/
 function onScoreSubmit(f) {
 	// var f = document.scoreFom으로 접근가능
-	var	name = f.name.value
-	var	kor = f.kor.value
-	var	eng = f.eng.value
-	var	math = f.math.value
+	var	name = f.name.value.trim() //trim 공백제거
+	var	kor = f.kor.value.trim()
+	var	eng = f.eng.value.trim()
+	var	math = f.math.value.trim()
 	if(name ===''){
 		alert('이름을 입력하세요.')
+		f.name.focus()
 		return false //중복 알림이 안 뜨도록
 	}
 	if(kor ===''){
 		alert('국어 점수를 입력하세요.')
+		f.kor.focus()
 		return false
 	}
 	if(eng ===''){
 		alert('영어 점수를 입력하세요.')
+		f.eng.focus()
 		return false
 	}
 	return false
