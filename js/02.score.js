bb020472fa9e57c4fce04058f373542930b10b36
Rimi999 <all_wom@naver.com>
2021-06-02 16:06:01 +0900

    value 받기

--- js/02.score.js
+++ js/02.score.js
@@ -1,1 +1,20 @@
+/***************** 글로벌 설정 *******************/
 var scores = []
+
+
+/***************** 사용자 함수 *******************/
+
+
+/***************** 이벤트 등록 *******************/
+
+
+/***************** 이벤트 콜백 *******************/
+function onScoreSubmit(f) {
+	// var f = document.scoreFom으로 접근가능
+	var	name = f.name.value
+	var	kor = Number(f.kor.value)
+	var	eng = Number(f.eng.value)
+	var	math = Number(f.math.value)
+	
+	return false
+}
