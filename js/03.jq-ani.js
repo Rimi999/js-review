38dad5af1f63a39fc9c31153ea3a74023dbe7162
Rimi999 <all_wom@naver.com>
2021-06-03 20:43:22 +0900

    .stop()

--- js/03.jq-ani.js
+++ js/03.jq-ani.js
@@ -1,7 +1,7 @@
 $('.bt-show').click(function() {
-	$('.hero').show(1000)
-})
+	$('.hero').stop().show(1000)
+}) //.stop()은 쌓인 액션이 반복 되지않도록 함
 
 $('.bt-hide').on('click', function() {
-	$('.hero').hide(1000)
+	$('.hero').stop().hide(1000)
 })
