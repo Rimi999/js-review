ccee18b250771b1c412fb6d7b277c3efd0484801
Rimi999 <all_wom@naver.com>
2021-06-09 23:41:32 +0900

    interval

--- js/05.slide1.js
+++ js/05.slide1.js
@@ -23,15 +23,24 @@ a = (조건)? 값1  : 값2
 var idx = 0  //현재 맨 위에 나타나는 .slide의 index
 var zIdx = 2 //맨 위에 나타나는 .slide의 z-index
 var lastIdx = $('.slide-wrap .slide').length - 1
-$('.slide-wrap .slide').eq(idx).css('z-index', zIdx++)
+var interval
+var intervalGap = 4000
+init()
 
 
 /*************** 사용자 함수 *****************/
+function init() {
+	$('.slide-wrap .slide').eq(idx).css('z-index', zIdx++)
+	var interval = setInterval(onNextClick, intervalGap)
+}
 
 
 /*************** 이벤트 등록 *****************/
 $('.slide-stage .bt-prev').on('click', onPrevClick)
 $('.slide-stage .bt-next').on('click', onNextClick)
+
+$('.slide-stage').on('mouseover', onOver)
+$('.slide-stage').on('mouseleave', onLeave)
 
 
 /*************** 이벤트 콜백 *****************/
@@ -45,4 +54,12 @@ function onNextClick(){
 	idx = (idx === lastIdx) ? 0 : idx + 1
 	$('.slide-wrap .slide').eq(idx).css('z-index', zIdx++)
 	.stop().fadeOut(0).fadeIn(500)
+}
+
+function onOver() {
+	clearInterval(interval)
+}
+
+function onLeave() {
+	interval = setInterval(onNextClick, intervalGap)
 }
