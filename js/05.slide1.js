cecf5bb650bd3c082914fa4ddfa73178232201a1
Rimi999 <all_wom@naver.com>
2021-06-09 19:33:53 +0900

    length

--- js/05.slide1.js
+++ js/05.slide1.js
@@ -14,10 +14,13 @@ console.log(++i) // 1
 console.log(++i) // 2
 console.log(++i) // 3
 */
+/*
 
+*/ 
 /*************** 글로벌 설정 *****************/
 var idx = 0  //현재 맨 위에 나타나는 .slide의 index
 var zIdx = 2 //맨 위에 나타나는 .slide의 z-index
+var lastIdx = $('.slide-wrap .slide').length - 1
 $('.slide-wrap .slide').eq(idx).css('z-index', zIdx++)
 
 
@@ -31,13 +34,13 @@ $('slide-stage .bt-next').on('click', onNextClick)
 
 /*************** 이벤트 콜백 *****************/
 function onPrevClick(){
-	if(idx === 0) idx = 5
+	if(idx === 0) idx = lastIdx
 	else idx--
 	('.slide-wrap .slide').eq(idx).css('z-index', zIdx++)
 }
 
 function onNextClick(){
-	if(idx === 5) idx = 0
+	if(idx === lastIdx) idx = 0
 	else idx++
 	('.slide-wrap .slide').eq(idx).css('z-index', zIdx++)
 }
