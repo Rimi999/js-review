894319cebeb83a53af293d6d0ab64ba7794d5395
Rimi999 <all_wom@naver.com>
2021-06-10 11:54:12 +0900

    수정완료

--- js/06.data.js
+++ js/06.data.js
@@ -9,12 +9,13 @@ init()
 function init(){
 	prds = [
 		{ id: 1, src: '../img/cherries.jpg', title: '맛있는 체리', desc: '체리'},
-		{ id: 1, src: '../img/croissant.jpg', title: '맛있는 빵', desc: '빵'},
-		{ id: 1, src: '../img/popsicle.jpg', title: '맛있는 크림', desc: '크림'},
-		{ id: 1, src: '../img/salmon.jpg', title: '맛있는 연어', desc: '연어'},
-		{ id: 1, src: '../img/sandowich.jpg', title: '맛있는 샌드위치', desc: '샌드위치'},
-		{ id: 1, src: '../img/steak.jpg', title: '맛있는 스테이크', desc: '스테이크'},
-		{ id: 1, src: '../img/wine.jpg', title: '맛있는 와인', desc: '와인'}
+		{ id: 2, src: '../img/croissant.jpg', title: '맛있는 빵', desc: '빵'},
+		{ id: 3, src: '../img/popsicle.jpg', title: '맛있는 크림', desc: '크림'},
+		{ id: 4, src: '../img/salmon.jpg', title: '맛있는 연어', desc: '연어'},
+		{ id: 5, src: '../img/sandowich.jpg', title: '맛있는 샌드위치', desc: '샌드위치'},
+		{ id: 6, src: '../img/steak.jpg', title: '맛있는 스테이크', desc: '스테이크'},
+		{ id: 7, src: '../img/wine.jpg', title: '맛있는 와인', desc: '와인'},
+		{ id: 8, src: '../img/popsicle.jpg', title: '맛있는 크림', desc: '크림'}
 	]
 	$listWrap = $('.list-wrap')
 }
@@ -28,14 +29,17 @@ $('.bt-remove').click(onRemove)
 /*************** 이벤트 콜백 *****************/
 function onCreate() {
 	var html
-	html += '<li class="list card">'
-	html += '<img class="card-img-top" src="../img/300.png">'
-	html += '<div class="card-body">'
-	html += '<h4 class="card-title">Title</h4>'
-	html += '<p class="card-text">Text</p>'
-	html += '</div>'
-	html += '</li>'
-	$listWrap.append(html)
+	for(var i=0; i<prds.length; i++ ) {
+		html = '<li class="list card">'
+		html += '	<img class="card-img-top" src="../img/300.png">'
+		html += '	<div class="card-body">'
+		html += '		<h4 class="card-title">Title</h4>'
+		html += '		<p class="card-text">Text</p>'
+		html += '	</div>'
+		html += '</li>'
+		$listWrap.append(html)
+	}
+	
 }
 
 function onRemove() {
